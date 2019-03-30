4a895462a1771ddc04c5d93191c4410066b87b3c
Gregor Seiler <gseiler@posteo.de>
2019-03-30 12:04:48 +0100

    added config.h

---
+++ ref/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#ifndef MODE
+#define MODE 3
+#endif
+
+//#define USE_AES
+//#define RANDOMIZED_SIGNING
+//#define USE_RDPMC
+//#define SERIALIZE_RDC
+//#define DBENCH
+
+#endif
