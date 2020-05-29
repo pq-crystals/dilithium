76035bab2326c372b8d58bd223c9786fe7d89f1f
Gregor Seiler <gseiler@posteo.de>
2020-05-29 21:46:14 +0200

    properly align seed buffer in test_speed.c

--- avx2/test/test_speed.c
+++ avx2/test/test_speed.c
@@ -1,1 +1,95 @@
-../../ref/test/test_speed.c
+#include <stdint.h>
+#include "../sign.h"
+#include "../poly.h"
+#include "../polyvec.h"
+#include "../params.h"
+#include "cpucycles.h"
+#include "speed_print.h"
+
+#define NTESTS 10000
+
+uint64_t t[NTESTS];
+
+int main(void)
+{
+  unsigned int i;
+  unsigned long long mlen, smlen;
+  unsigned char pk[CRYPTO_PUBLICKEYBYTES];
+  unsigned char sk[CRYPTO_SECRETKEYBYTES];
+  __attribute__((aligned(32)))
+  unsigned char sm[CRYPTO_BYTES + CRHBYTES] = {0};
+  unsigned char *m = sm;
+  uint8_t *seed = sm;
+  polyvecl mat[K];
+  poly *a = &mat[0].vec[0];
+  poly *b = &mat[0].vec[1];
+  poly *c = &mat[0].vec[2];
+
+  for(i = 0; i < NTESTS; ++i) {
+    t[i] = cpucycles();
+    expand_mat(mat, seed);
+  }
+  print_results("expand_mat:", t, NTESTS);
+
+/*
+  unsigned int j;
+  polyvecl *vl = &mat[0];
+  for(i = 0; i < NTESTS; ++i) {
+    t[i] = cpucycles();
+    for(j = 0; j < L; ++j)
+      poly_uniform_eta(&vl->vec[j], seed, j);
+  }
+  print_results("polyvecl_uniform_eta:", t, NTESTS);
+
+  for(i = 0; i < NTESTS; ++i) {
+    t[i] = cpucycles();
+    for(j = 0; j < L; ++j)
+      poly_uniform_gamma1m1(&vl->vec[j], seed, j);
+  }
+  print_results("polyvecl_uniform_gamma1m1:", t, NTESTS);
+*/
+
+  for(i = 0; i < NTESTS; ++i) {
+    t[i] = cpucycles();
+    poly_ntt(a);
+  }
+  print_results("poly_ntt:", t, NTESTS);
+
+  for(i = 0; i < NTESTS; ++i) {
+    t[i] = cpucycles();
+    poly_invntt_tomont(a);
+  }
+  print_results("poly_invntt_tomont:", t, NTESTS);
+
+  for(i = 0; i < NTESTS; ++i) {
+    t[i] = cpucycles();
+    poly_pointwise_montgomery(c, a, b);
+  }
+  print_results("poly_pointwise_montgomery:", t, NTESTS);
+
+  for(i = 0; i < NTESTS; ++i) {
+    t[i] = cpucycles();
+    challenge(c, seed, (polyveck *)mat);
+  }
+  print_results("challenge:", t, NTESTS);
+
+  for(i = 0; i < NTESTS; ++i) {
+    t[i] = cpucycles();
+    crypto_sign_keypair(pk, sk);
+  }
+  print_results("Keypair:", t, NTESTS);
+
+  for(i = 0; i < NTESTS; ++i) {
+    t[i] = cpucycles();
+    crypto_sign(sm, &smlen, m, CRHBYTES, sk);
+  }
+  print_results("Sign:", t, NTESTS);
+
+  for(i = 0; i < NTESTS; ++i) {
+    t[i] = cpucycles();
+    crypto_sign_open(m, &mlen, sm, smlen, pk);
+  }
+  print_results("Verify:", t, NTESTS);
+
+  return 0;
+}
