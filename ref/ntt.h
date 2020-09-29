70029a89fa5defcd7d343b0b0d747feef40a08aa
Gregor Seiler <gseiler@posteo.de>
2020-09-29 15:26:04 +0200

    ntt with signed arithmetic and no separate consts for invntt

--- ref/ntt.h
+++ ref/ntt.h
@@ -5,9 +5,9 @@
 #include "params.h"
 
 #define ntt DILITHIUM_NAMESPACE(_ntt)
-void ntt(uint32_t p[N]);
+void ntt(int32_t a[N]);
 
 #define invntt_tomont DILITHIUM_NAMESPACE(_invntt_tomont)
-void invntt_tomont(uint32_t p[N]);
+void invntt_tomont(int32_t a[N]);
 
 #endif
