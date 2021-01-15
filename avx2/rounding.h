d103a544ee9b460822f42ed3a37696fdfab5f5b8
Gregor Seiler <gseiler@posteo.de>
2021-01-15 10:03:04 +0100

    immediately convert hint coefficients to index representation in vectorized code

--- avx2/rounding.h
+++ avx2/rounding.h
@@ -2,15 +2,16 @@
 #define ROUNDING_H
 
 #include <stdint.h>
+#include <immintrin.h>
 #include "params.h"
 
 #define power2round_avx DILITHIUM_NAMESPACE(_power2round_avx)
-void power2round_avx(int32_t a1[N], int32_t a0[N], const int32_t a[N]);
+void power2round_avx(__m256i *a1, __m256i *a0, const __m256i *a);
 #define decompose_avx DILITHIUM_NAMESPACE(_decompose_avx)
-void decompose_avx(int32_t a1[N], int32_t a0[N], const int32_t a[N]);
+void decompose_avx(__m256i *a1, __m256i *a0, const __m256i *a);
 #define make_hint_avx DILITHIUM_NAMESPACE(_make_hint_avx)
-unsigned int make_hint_avx(int32_t h[N], const int32_t a0[N], const int32_t a1[N]);
+unsigned int make_hint_avx(uint8_t hint[N], const __m256i *a0, const __m256i *a1);
 #define use_hint_avx DILITHIUM_NAMESPACE(_use_hint_avx)
-void use_hint_avx(int32_t b[N], const int32_t a[N], const int32_t hint[N]);
+void use_hint_avx(__m256i *b, const __m256i *a, const __m256i *hint);
 
 #endif
