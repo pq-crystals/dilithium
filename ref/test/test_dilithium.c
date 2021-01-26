bda9689704262b53f0942efdda6340b922acb58b
Gregor Seiler <gseiler@posteo.de>
2021-01-26 10:19:39 +0100

    use top-level LICENSE in .yml-files

--- ref/test/test_dilithium.c
+++ ref/test/test_dilithium.c
@@ -9,12 +9,13 @@
 
 int main(void)
 {
-  unsigned int i, j;
+  size_t i, j;
   int ret;
   size_t mlen, smlen;
-  uint8_t m[MLEN] = {0};
-  uint8_t sm[MLEN + CRYPTO_BYTES];
+  uint8_t b;
+  uint8_t m[MLEN + CRYPTO_BYTES];
   uint8_t m2[MLEN + CRYPTO_BYTES];
+  uint8_t sm[MLEN + CRYPTO_BYTES];
   uint8_t pk[CRYPTO_PUBLICKEYBYTES];
   uint8_t sk[CRYPTO_SECRETKEYBYTES];
 
@@ -29,14 +30,16 @@ int main(void)
       fprintf(stderr, "Verification failed\n");
       return -1;
     }
-
-    if(mlen != MLEN) {
-      fprintf(stderr, "Message lengths don't match\n");
+    if(smlen != MLEN + CRYPTO_BYTES) {
+      fprintf(stderr, "Signed message lengths wrong\n");
       return -1;
     }
-
-    for(j = 0; j < mlen; ++j) {
-      if(m[j] != m2[j]) {
+    if(mlen != MLEN) {
+      fprintf(stderr, "Message lengths wrong\n");
+      return -1;
+    }
+    for(j = 0; j < MLEN; ++j) {
+      if(m2[j] != m[j]) {
         fprintf(stderr, "Messages don't match\n");
         return -1;
       }
@@ -44,9 +47,9 @@ int main(void)
 
     randombytes((uint8_t *)&j, sizeof(j));
     do {
-      randombytes(m2, 1);
-    } while(!m2[0]);
-    sm[j % CRYPTO_BYTES] += m2[0];
+      randombytes(&b, 1);
+    } while(!b);
+    sm[j % (MLEN + CRYPTO_BYTES)] += b;
     ret = crypto_sign_open(m2, &mlen, sm, smlen, pk);
     if(!ret) {
       fprintf(stderr, "Trivial forgeries possible\n");
