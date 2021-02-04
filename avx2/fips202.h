71c1e9e912a6eb9bc7a40c9892aa4a382c968ead
Basil Hess <bhe@zurich.ibm.com>
2021-02-04 11:14:51 +0100

    - Added Commons_META.yml to define common dependencies for implementations and parameter sets. (#39)
    - Made namespaces for fips202, fips202x4, aes256ctr etc more distinctive to avoid collisions when both kyber and dilithium are linked at the same time.

--- avx2/fips202.h
+++ avx2/fips202.h
@@ -9,7 +9,7 @@
 #define SHA3_256_RATE 136
 #define SHA3_512_RATE 72
 
-#define FIPS202_NAMESPACE(s) pqcrystals_fips202_avx2_##s
+#define FIPS202_NAMESPACE(s) pqcrystals_dilithium_fips202_avx2_##s
 
 typedef struct {
   uint64_t s[25];
